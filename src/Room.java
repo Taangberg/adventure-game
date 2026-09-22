22f71f3fff83fc95017af7da1dc2776593b30538
almaexner <alje0003@stud.ek.dk>
2026-09-22 15:27:24 +0200

    Room class, constructor and getter and setters.

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,54 @@
 public class Room {
+    String name;
+    String description;
+    Room north;
+    Room east;
+    Room south;
+    Room west;
+
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Room setNorth(Room room) {
+        return room;
+    }
+
+    public Room getNorth() {
+        return Room;
+    }
+
+    public Room setSouth(Room room) {
+        return room;
+    }
+
+    public Room getSouth() {
+        return Room;
+    }
+
+    public Room setEast(Room room) {
+        return room;
+    }
+
+    public Room getEast() {
+        return Room;
+    }
+
+    public Room setWest(Room room) {
+        return room;
+    }
+
+    public Room getWest() {
+        return Room;
+    }
+
 }
