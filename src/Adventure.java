56390d93d5817d2a27c897869ea9e4e18daaaddf
emiltaangberg <taangberg.e@gmail.com>
2026-09-23 09:43:48 +0200

    Added extra cases

--- src/Adventure.java
+++ src/Adventure.java
@@ -2,8 +2,9 @@ public class Adventure {
     boolean gameIsRunning = true;
 
     public void startGame() {
-        IO.println("Welcome to game");
-        IO.println("Type which direction you would like to go");
+        IO.println("Welcome to game + description of game\n");
+        String name = IO.readln("What is your name? ");
+        IO.println("\nHello "+ name + "\nTo move around the labyrinth, type: 'GO' + direction");
 
         while (gameIsRunning) {
 
@@ -25,6 +26,21 @@ public class Adventure {
                 case "GO EAST" -> {
                     IO.println("Going east");
                     IO.println("Arrived at added room");
+                }
+                case "LOOK" -> {
+                    IO.println("You are in currentRoom");
+                    IO.println("Looking around");
+                    IO.println("Description of currentRoom");
+                }
+                case "HELP" -> {
+                    IO.println("To move around type: 'GO' + direction");
+                    IO.println("To get information about the current room, type: 'LOOK'");
+                    IO.println("To stop the current game type: 'EXIT'");
+                }
+                case "EXIT" -> {
+                    IO.println("Thanks for playing.");
+                    IO.println("Goodbye");
+                    gameIsRunning = false;
                 }
             }
         }
