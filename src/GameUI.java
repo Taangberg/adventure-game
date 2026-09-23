f3c05bf2987e8eef023fc32be239ec653e7fbffd
emiltaangberg <taangberg.e@gmail.com>
2026-09-23 10:52:35 +0200

    If statements added to directions

--- src/GameUI.java
+++ src/GameUI.java
@@ -14,22 +14,38 @@ public class GameUI {
             switch (commando) {
                 case "GO NORTH" -> {
                     IO.println("Going north!");
+                    if (!adventure.goNorth){
+                        IO.println("There is no way for me to go north");
+                        return;
+                    }
                     IO.println("Arrived at added room");
                 }
                 case "GO SOUTH" -> {
                     IO.println("Going south");
+                    if (!adventure.goSouth){
+                        IO.println("There is no way for me to go south");
+                        return;
+                    }
                     IO.println("Arrived at added room");
                 }
                 case "GO WEST" -> {
                     IO.println("Going west");
+                    if (!adventure.goWest){
+                        IO.println("There is no way for me to go west");
+                        return;
+                    }
                     IO.println("Arrived at added room");
                 }
                 case "GO EAST" -> {
                     IO.println("Going east");
+                    if (!adventure.goEast){
+                        IO.println("There is no way for me to go east");
+                        return;
+                    }
                     IO.println("Arrived at added room");
                 }
                 case "LOOK" -> {
-                    IO.println(currentRoom);
+                    IO.println();
                 }
                 case "HELP" -> {
                     IO.println("To move around type: 'GO' + direction");
